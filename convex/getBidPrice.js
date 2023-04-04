5f38389e9c60a621407681652cf23bc5e90e84e4
Tao707499 <ty2481@columbia.edu>
2023-04-04 15:50:02 -0400

    First version

---
+++ convex/getBidPrice.js
@@ -0,0 +1,11 @@
+// define the UDF here
+import { query } from "./_generated/server";
+
+export default query(async ({ db }) => {
+  return await db.query("messages").collect();
+
+
+
+
+  
+});
--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,33 @@ export default function App() {
   const sendMessage = useMutation("sendMessage");
 
   const [name] = useState(() => "User " + Math.floor(Math.random() * 10000));
+
   async function handleSendMessage(event) {
     event.preventDefault();
     setNewMessageText("");
     await sendMessage({ body: newMessageText, author: name });
   }
+
+  // Add a new query to retrieve the bid price
+  //const bidPrice = useQuery("getBidPrice");
+
+  const bidPrice = "100";
+
   return (
     <main>
       <h1>Convex Chat</h1>
       <p className="badge">
         <span>{name}</span>
       </p>
+
+      <div id="artwor_image" align="center">
+        <img src="https://www.ss.net.tw/images/product_images/popup_images/VanGogh008.webp"
+        alt="image01.jpg" width="300px" title="The Starry Night"></img>
+      </div>
+
+      {/* Render the bid price based on the response of the `getBidPrice` query */}
+      <div id="bid_price" align="center">Current bid price: {bidPrice || "1,000,000"}$</div>
+
       <ul>
         {messages.map(message => (
           <li key={message._id.toString()}>
@@ -28,6 +44,7 @@ export default function App() {
           </li>
         ))}
       </ul>
+
       <form onSubmit={handleSendMessage}>
         <input
           value={newMessageText}
@@ -36,6 +53,7 @@ export default function App() {
         />
         <input type="submit" value="Send" disabled={!newMessageText} />
       </form>
+
     </main>
   );
 }
